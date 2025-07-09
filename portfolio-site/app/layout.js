4a72b32a184fdd8d285b4b8e9075c3bdfc330c7e
dganjali <64025829+dganjali@users.noreply.github.com>
2025-07-08 22:04:07 -0400

    yuh

--- portfolio-site/app/layout.js
+++ portfolio-site/app/layout.js
@@ -21,6 +21,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/app/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
